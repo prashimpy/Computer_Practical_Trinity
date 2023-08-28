cfd9f934962db77a8130744cf951a02ec50548e4
Prashim-hack <prashim.py@gmail.com>
2023-08-28 19:35:43 +0545

    File handling complete

--- Computer Practical XII/C-programming/File Handling/file7/file7.c
+++ Computer Practical XII/C-programming/File Handling/file7/file7.c
@@ -1,26 +1,117 @@
 //WAP to show the concept of rename() and remove() functions.
-#include <stdio.h>
+// to update data in a data file
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
 
-int main() {
+struct employee {
+	int id;
+	char n[100];
+	char d[100];
+	int s;
+	
+}e;
+
+int main(){
 	
 	printf("Author: \t Prashim Timsina. \n");
-	printf("Program: \t Use rename() and remove().\n\n");
+	printf("Program: \t Use fwrite() and fread().\n\n");
 	
-    char oldFileName[] = "flie7.txt";   // Existing file name
-    char newFileName[] = "file7.txt";   // New file name
-
-    if (rename(oldFileName, newFileName) == 0) {
-        printf("File renamed successfully.\n");
-    } else {
-        perror("Error renaming the file");
-    }
-    
-    if (remove(newFileName) == 0) {
-        printf("File removed successfully.\n");
-    } else {
-        perror("Error re the file");
-    }
-
-    return 0;
+	FILE *p;
+	FILE *p1;
+	char v;
+	int tid,ts;
+	char tn[100],td[100];
+	
+	p = fopen("file7.txt","w");
+	
+	printf("-----------------------\n");
+	printf("Enter data in file.... \n");
+	printf("-----------------------\n");
+	do{
+		printf("\n");
+		
+		printf("Enter Employee Id: ");
+		scanf("%d",&e.id);
+		
+		printf("Enter Employee Name: ");
+		scanf("%s",e.n);
+		
+		printf("Enter Employee Designation: ");
+		scanf("%s",e.d);
+		
+		printf("Enter Employee Salary: ");
+		scanf("%d",&e.s);
+		
+		fwrite(&e, sizeof(struct employee), 1, p);
+		
+		printf("\nDo you want to continue?(y/n): ");
+		v = getche();
+		printf("\n");
+		
+	}while(v != 'n');
+	
+	fclose(p);
+	
+	p = fopen("file7.txt","r");
+	p1= fopen("file7temp.txt","w");
+	
+	printf("\n---------------------------");
+	printf("\nThe Stored data is.... \n");
+	printf("---------------------------\n");
+	while(fread(&e, sizeof(struct employee),1, p)){
+		printf("\nId: %d", e.id);
+		printf("\nName: %s",e.n);
+		printf("\nDesignation: %s",e.d);
+		printf("\nName: %d",e.s);
+		printf("\n");
+	}
+	
+	printf("\n---------------------------");
+	printf("\nUpdating Data.... \n");
+	printf("---------------------------\n");
+	
+	printf("\nEnter Id of data to be updated: ");
+	scanf("%d", &tid);
+	
+	rewind(p);
+	//to update
+	while(fread(&e, sizeof(struct employee),1, p)){
+		if(e.id == tid){
+			printf("\nData found successfully!!!\n\n");
+			printf("Enter Updated Name: ");
+			scanf("%s",e.n);
+			printf("Enter Updated Designation: ");
+			scanf("%s", e.d);
+			printf("Enter Updated Salary: ");
+			scanf("%d", &e.s);
+			fwrite(&e, sizeof(struct employee), 1, p1);
+		}
+		else{
+			fwrite(&e, sizeof(struct employee), 1, p1);	
+		}
+		printf("\n");
+	}
+	
+	fclose(p);
+	fclose(p1);
+	
+	remove("file7.txt");
+	rename("file7temp.txt","file7.txt");
+	
+	p = fopen("file7.txt","r");
+	
+	printf("\n---------------------------");
+	printf("\nThe Updated data is.... \n");
+	printf("---------------------------\n");
+	rewind(p);
+	while(fread(&e, sizeof(struct employee),1, p)){
+		printf("\nName: %s",e.n);
+		printf("\nDesignation: %s",e.d);
+		printf("\nName: %d",e.s);
+		printf("\n");
+	}
+	fclose(p);
+	return 0;
+	
 }
-
